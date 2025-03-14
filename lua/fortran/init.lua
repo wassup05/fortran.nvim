7cd18dcf1c66fe0253210a51d42ade4d1503e51e
supritsj@Arch <supritsj05@gmail.com>
2025-03-14 18:27:46 +0530

    formatting

--- lua/fortran/init.lua
+++ lua/fortran/init.lua
@@ -7,59 +7,59 @@ local prefix = "fortan.nvim: "
 M.server = true
 
 M.defaults = {
-	server_opts = {
-		path = "fortls",
-		args = {
-			"--notify_init",
-			"--lowercase_intrinsics",
-			"--hover_signature",
-			"--hover_language=fortran",
-			"--use_signature_help",
-			"--enable_code_actions",
-		},
-		filetypes = { "fortran" },
-		settings = {},
-		single_file_support = true,
-	},
+  server_opts = {
+    path = "fortls",
+    args = {
+      "--notify_init",
+      "--lowercase_intrinsics",
+      "--hover_signature",
+      "--hover_language=fortran",
+      "--use_signature_help",
+      "--enable_code_actions",
+    },
+    filetypes = { "fortran" },
+    settings = {},
+    single_file_support = true,
+  },
 
-	fpm_opts = {
-		path = "fpm",
-		args = {},
-	},
+  fpm_opts = {
+    path = "fpm",
+    args = {},
+  },
 
-	formatter_opts = {
-		path = "fprettify",
-		format_on_save = true,
-		args = {},
-	},
+  formatter_opts = {
+    path = "fprettify",
+    format_on_save = true,
+    args = {},
+  },
 }
 
 M.setup = function(opts)
-	M.handle_opts(opts)
-	M.check_requirements(M._opts)
-	cmd.setup_commands(M._opts)
+  M.handle_opts(opts)
+  M.check_requirements(M._opts)
+  cmd.setup_commands(M._opts)
   cmd.setup_format_autocmd(M._opts.formatter_opts)
 
   if M.server then
-	  server.start_server(M._opts)
+    server.start_server(M._opts)
   end
 end
 
 M.handle_opts = function(opts)
-	M.user_opts = opts
-	M._opts = vim.tbl_deep_extend("force", M.defaults, opts)
+  M.user_opts = opts
+  M._opts = vim.tbl_deep_extend("force", M.defaults, opts)
 end
 
 M.check_requirements = function(opts)
-	local exists = vim.fn.executable
-	local warn = vim.log.levels.WARN
+  local exists = vim.fn.executable
+  local warn = vim.log.levels.WARN
 
-	if exists(opts.server_opts.path) == 0 then
-		vim.notify(prefix .. "server " .. opts.server_opts.path .. " not found", warn)
+  if exists(opts.server_opts.path) == 0 then
+    vim.notify(prefix .. "server " .. opts.server_opts.path .. " not found", warn)
     M.server = false
-	elseif exists(opts.fpm_opts.path) == 0 then
-		vim.notify(prefix .. "package manager " .. opts.fpm_opts.path .. " not found", warn)
-	end
+  elseif exists(opts.fpm_opts.path) == 0 then
+    vim.notify(prefix .. "package manager " .. opts.fpm_opts.path .. " not found", warn)
+  end
 end
 
 return M
--- lua/fortran/server.lua
+++ lua/fortran/server.lua
@@ -1,27 +1,27 @@
 local M = {}
 
 M.start_server = function(opts)
-	local lsp = require("lspconfig")
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
+  local lsp = require("lspconfig")
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
 
   table.insert(opts.server_opts.args, 1, opts.server_opts.path)
 
-	if opts.server_opts.single_file_support then
-		lsp.fortls.setup({
-			capabilities = capabilities,
-			settings = opts.server_opts.settings,
-			filetypes = opts.server_opts.filetypes,
-			cmd = opts.server_opts.cmd,
-			root_dir = vim.fn.getcwd(),
-		})
-	end
+  if opts.server_opts.single_file_support then
+    lsp.fortls.setup({
+      capabilities = capabilities,
+      settings = opts.server_opts.settings,
+      filetypes = opts.server_opts.filetypes,
+      cmd = opts.server_opts.cmd,
+      root_dir = vim.fn.getcwd(),
+    })
+  end
 
-	lsp.fortls.setup({
-		capabilities = capabilities,
-		settings = opts.server_opts.settings,
-		filetypes = opts.server_opts.filetypes,
-		cmd = opts.server_opts.cmd,
-	})
+  lsp.fortls.setup({
+    capabilities = capabilities,
+    settings = opts.server_opts.settings,
+    filetypes = opts.server_opts.filetypes,
+    cmd = opts.server_opts.cmd,
+  })
 end
 
 return M
