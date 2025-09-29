318da7638932ebd05cb3cfc16c8b7e0bf5a2b508
Felipe Faria <65261523+Faria22@users.noreply.github.com>
2025-09-29 11:32:07 -0400

    Update server.lua to use vim.lsp.config

--- lua/fortran/server.lua
+++ lua/fortran/server.lua
@@ -1,17 +1,17 @@
 local M = {}
 
 M.start_server = function(opts)
-  local lsp = require("lspconfig")
   local capabilities = vim.lsp.protocol.make_client_capabilities()
 
   table.insert(opts.server_opts.args, 1, opts.server_opts.path)
 
-  lsp.fortls.setup({
+  vim.lsp.config('fortls', {
     capabilities = capabilities,
     settings = opts.server_opts.settings,
     filetypes = opts.server_opts.filetypes,
     cmd = opts.server_opts.cmd,
   })
+  vim.lsp.enable('fortls')
 end
 
 return M
